3e992ac152fcdf0091ee18c8d7777ce214ca4672
Timothy <tbarnes62@austin.rr.com>
2018-08-08 17:16:34 -0500

    Testing

--- public/app.js
+++ public/app.js
@@ -32,19 +32,6 @@ let gameTime;
 
 database.ref('games').on('value', function(snapshot) {
   // console.log(snapshot.val().inProgress)
-
-  while(snapshot.val().inProgress === true){
-    for(let x =1; x < p1_wordArray.length; x++){
-      let p1_Word = document.createElement('li')
-      p1_Word.innerText = p1_wordArray[x]
-      p1_wordList.appendChild(p1_Word)
-    }
-    for(let y = 1; y < p2_wordArray.length; y++){
-      let p2_Word = document.createElement('li')
-      p2_Word.innerText = p2_wordArray[y]
-      p2_wordList.appendChild(p2_Word)
-    }
-  }
     if(snapshot.val().inProgress === true){
       for(let x = 0; x < gameLetters.length; x++){
         gameLetters[x].innerHTML = snapshot.val().gameLetters[x];
@@ -54,6 +41,17 @@ database.ref('games').on('value', function(snapshot) {
       }
       p1_wordArray = snapshot.val().playerOne.words
       p2_wordArray = snapshot.val().playerTwo.words
+
+      for(let x =1; x < p1_wordArray.length; x++){
+        let p1_Word = document.createElement('li')
+        p1_Word.innerText = p1_wordArray[x]
+        p1_wordList.appendChild(p1_Word)
+      }
+      for(let y = 1; y < p2_wordArray.length; y++){
+        let p2_Word = document.createElement('li')
+        p2_Word.innerText = p2_wordArray[y]
+        p2_wordList.appendChild(p2_Word)
+      }
 
 
 
